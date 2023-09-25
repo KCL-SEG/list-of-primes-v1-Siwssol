165b0e0a9ae6c7010664b620830708d6d49b5fec
Lewis Kai Ho Wong (k22027157) <lewis.wong@kcl.ac.uk>
2023-09-25 15:19:18 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
+import math
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    listOfPrimes = []
+    number = 2
+    while (len(listOfPrimes) < number_of_primes):
+        check = True
+        for a in range(1, math.ceil(number**0.5) + 1):
+            if a == 1 or a == number:
+                continue
+            if (number%a == 0):
+                check = False
+        if check:
+            listOfPrimes.append(number)
+        number += 1
+    return listOfPrimes
+
